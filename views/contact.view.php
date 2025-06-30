4df5c4e0b1ac9633a3b982b2c466928aaeaac95f
techtee19 <techtee19@gmail.com>
2025-06-30 12:15:35 +0100

    Add initial implementation of about, contact, and index pages with layout components

---
+++ views/contact.view.php
@@ -0,0 +1,12 @@
+<?php require 'layout/head.php' ?>
+
+  <?php require 'layout/nav.php' ?>
+
+  <?php require 'layout/banner.php' ?>
+
+  <main>
+    <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+        <p>  Hello, this is the  Contact page.</p>
+    </div>
+  </main>
+<?php require 'layout/foot.php' ?>
---
+++ views/index.view.php
@@ -0,0 +1,11 @@
+<?php require 'layout/head.php' ?>
+  <?php require 'layout/nav.php' ?>
+
+ <?php require 'layout/banner.php' ?>
+
+  <main>
+    <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+        <p> Hello, welcome to the home page </p>
+    </div>
+  </main>
+<?php require 'layout/foot.php' ?>
